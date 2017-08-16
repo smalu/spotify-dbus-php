1ecc6d996a19e2fa5c2f1542303573a227f7f8e7
Mikołaj Smal <ja@smalu.info>
2017-08-16 22:37:43 +0200

    playUri method with example, added timeout handling for dbus-send

---
+++ examples/play_song.php
@@ -0,0 +1,8 @@
+<?php
+
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use smalu\SpotifyDBus;
+
+SpotifyDBus\SpotifyCommand::create()->playUri('spotify:track:2VGTe5PkHOlZuayLYDjtb1');
--- src/SpotifyCommand.php
+++ src/SpotifyCommand.php
@@ -4,16 +4,13 @@ namespace smalu\SpotifyDBus;
 
 use AdamBrett\ShellWrapper\Command;
 use AdamBrett\ShellWrapper\Command\SubCommand;
-use AdamBrett\ShellWrapper\Command\Param;
-use AdamBrett\ShellWrapper\Command\Argument;
 use AdamBrett\ShellWrapper\Runners\Exec;
-use AdamBrett\ShellWrapper\Runners\DryRunner;
 
 
 class SpotifyCommand {
 	
 	
-	const DBUS_COMMAND		= 'dbus-send';
+	const DBUS_COMMAND		= 'timeout 1 dbus-send';
 	const DBUS_DEST_PLAYER	= 'org.mpris.MediaPlayer2.Player';
 	const DBUS_DEST_SPOTIFY	= 'org.mpris.MediaPlayer2.spotify';
 	const DBUS_PATH			= '/org/mpris/MediaPlayer2';
@@ -63,6 +60,21 @@ class SpotifyCommand {
 		$this->run();
 	}
 	
+	public function playUri($uri){
+		
+		if($uri === NULL){
+			
+			throw new \InvalidArgumentException('No spotify URI provided');
+			
+		}
+		
+		$this->command->addSubCommand(new SubCommand('org.mpris.MediaPlayer2.Player.OpenUri'));
+		$this->command->addSubCommand(new SubCommand('string:' . $uri));
+		
+		$this->run();
+
+	}
+	
 	public function getMetadata(){
 		
 		$this->command->addSubCommand(new SubCommand('org.freedesktop.DBus.Properties.Get'));
@@ -79,12 +91,18 @@ class SpotifyCommand {
 		
 		$this->shell->run($this->command);
 		
+		
 		if($this->shell->getReturnValue() === 1){
 			
 			throw new \Exception('Could not connect to Spotify via DBus');
 		
-		}
+		}else if($this->shell->getReturnValue() === 124){
+			
+			// GNU timeout default exit code for timeouted scripts
+			
+			throw new \Exception('Could not connect to Spotify via DBus - timeout');
 		
+		}
 	}
 	
 	
